2173f135ce6129eff1bd63a60f624ae11f3dfedc
Jared Tan <jian.tan@daocloud.io>
2025-03-30 19:48:40 +0800

    add demo code

---
+++ go.mod
@@ -0,0 +1,18 @@
+module hello
+
+go 1.23.3
+
+require google.golang.org/grpc v1.61.0
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+)
+
+require (
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/protobuf v1.36.5
+)
---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "hello/pkg/hello" // 替换为实际的包路径
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
